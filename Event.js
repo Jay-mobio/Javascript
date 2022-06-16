c2d61d13a63e515b4190fecc3e78369e9d4ec74b
Jay Kapadiya <jayk.mobio@gmail.com>
2022-06-16 12:57:52 +0530

    Array

--- Event.js
+++ Event.js
@@ -1,6 +1,5 @@
 var btn = document.getElementById("go-button");
-console.log(btn)
-btn.addEventListener("click", buttonclicked());
+btn.addEventListener("click", buttonclicked())
 
 function buttonclicked() {
     console.log("Button clicked");
---
+++ array.js
@@ -0,0 +1,10 @@
+hobbies = ["eating","gaming","music","programming","reading"]
+hobbies.push("archery")
+hobbies.forEach(function(i,ie)
+{
+    console.log(i,ie);
+       
+});
+hobbies.shift();
+hobbies.unshift("blacksmith");
+
