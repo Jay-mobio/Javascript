cda1ea895f21c8d4289c512ebd5599361b8eeca9
Jay-mobio <107192836+Jay-mobio@users.noreply.github.com>
2022-06-17 16:45:15 +0530

    Add files via upload

---
+++ Oop-json.js
@@ -0,0 +1,7 @@
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+  };
+  let myString = JSON.stringify(person);
+  document.getElementById("demo").innerHTML = JSON.stringify(person);myString
